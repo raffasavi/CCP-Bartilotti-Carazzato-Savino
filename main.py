a10571222fda67f2e949a104dc45e8d5e2ff7f5f
Raffaele <233198@unimore.it>
2022-05-18 12:39:28 +0200

    lancio dell'app su gcp

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from flask import Flask, request
 from google.cloud import firestore
+from secret import secret
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
-    return 'ok'
+    return 'ok FUNZIONA ok'
 
-@app.route('/sensors/sensor', methods=['POST']) ## guardare per gli indirizzi personalizzati con <s>
+@app.route('/sensors/sensor', methods=['POST']) ## guardare per gli indirizzi personalizzati con
 def save_data():
-    idsens = request.values['idsens']
-    date = request.values['date']
-    hms = request.values['hms']
-    hour = request.values['hour']
-    value = request.values['value']
-    db = firestore.Client()
-    db.collection(idsens).document(hms).set({'date': date, 'hour':hour, 'value': value})
-    return 'ok', 200
-
+    s = request.values['secret']
+    if s == secret:
+        idsens = request.values['idsens']
+        date = request.values['date']
+        hms = request.values['hms']
+        hour = request.values['hour']
+        value = request.values['value']
+        db = firestore.Client()
+        db.collection(idsens).document(hms).set({'date': date, 'hour':hour, 'value': value})
+        return 'ok', 200
+    else:
+        return 'non autorizzato',401
 # funzione per mandare i dati a chi li chiede, infatti il metodo è GET
 @app.route('/sensors/sensor1', methods=['GET'])
 def get_data():
@@ -29,3 +33,4 @@ def get_data():
 # esecuzione flask sulla porta 8080
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
+
